5916d8c5b2c17aa2571c262ea7091f3cebee9d66
Adarshmcherupuzha <adarshmpadichal@gmail.com>
2024-02-08 21:55:14 +0530

    nav completed

---
+++ src/components/Add3.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav1 from './Nav1'
+
+const Add3 = () => {
+  return (
+    <div>
+        <Nav1/>
+        
+    </div>
+  )
+}
+
+export default Add3
---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav1 from './Nav1'
+
+const Search = () => {
+  return (
+    <div>
+        <Nav1/>
+
+        
+    </div>
+  )
+}
+
+export default Search
