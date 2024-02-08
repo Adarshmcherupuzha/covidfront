e2e76996a672503c088578ee9331b70671e8ce88
Adarshmcherupuzha <adarshmpadichal@gmail.com>
2024-02-08 22:22:09 +0530

    add and search created

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,8 +5,78 @@ const Search = () => {
   return (
     <div>
         <Nav1/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                    
+        <div className="row g-3">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">
+                    Name
+                </label>
+                <input type="text" className="form-control" />
+                <br/>
+                <button className="btn btn-success">Search</button>
+                
+            
+
+
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">
+                    Phone
+                </label>
+
+                <input type="text" className="form-control" />
+                <br/>
+                <button className="btn btn-success">Search</button>
+
+
+</div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">
+                    Address
+                </label>
+                <input type="text" className="form-control" />
+                <br/>
+                <button className="btn btn-success">Search</button>
+
+</div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <label htmlFor="" className="form-label">
+                    Symptom
+                </label>
+                <input type="text" className="form-control" />
+                <br/>
+                <button className="btn btn-success">Search</button>
+
+</div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                
+            <label htmlFor="" className="form-label">
+                    Status
+                </label>
+                 <input type="text" className="form-control" />
+                 <br/>
+                <button className="btn btn-success">Search</button>
+
+
+            </div>
+        </div>
+
+
+
+                </div>
+            </div>
+        </div>
 
         
+
+
+
+
+
     </div>
   )
 }
